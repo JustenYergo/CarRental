4755a534735e6d6e9e31359014f1d1721755c070
Justen Yergo <justenyergo@gmail.com>
2024-09-07 00:08:58 -0400

    background image, card, and some card content and form fields

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatNativeDateModule, MatDatepickerModule, MatFormFieldModule, MatInputModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
